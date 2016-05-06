d55798fad80d3127f0f3648e2f39b8540bcb7b4a
reetesh <reetesh@reetesh-Lenovo-IdeaPad-Y530>
2016-05-05 23:52:52 -0400

    updated

--- Java Project/src/com/java/Solution.java
+++ Java Project/src/com/java/Solution.java
@@ -10,8 +10,14 @@ public static void main(String[] args) {
         String A=sc.next();
         String B=sc.next();
         /* Enter your code here. Print output to STDOUT. */
-        System.out.println(A);
-        System.out.println(B);
-    }
+        System.out.println(A.length()+B.length());
+        if(A.length()>=B.length()){
+        	System.out.println("No");
+        }else{
+        	System.out.println("Yes");
+        }
+        System.out.println(A.substring(0, 1).toUpperCase()+A.substring(1)+" "+B.substring(0, 1).toUpperCase()+B.substring(1));
+    
+}
 
 }
